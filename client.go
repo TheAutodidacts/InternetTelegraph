c52116652eabc56ef613d54e1655b9d0197d3a4e
Curiositry <isa@automathic.org>
2017-04-16 12:19:34 -0700

    This whopping commit contains way too many changes. Among them: =======================================================
    - Move websocket connection to its own dial function
    - Add code to key and websocket listen functions to redial websocket if sending / receiving fails
    - Add function that translates dots and dashes to ~20wpm morse code, for playing key status messages (ready, error, etc)
    - Rename rooms to channels, and change path to /channels/yourchannel
    - Change timing of key listen loop (still need to switch to interrrupt polling, but not as urgent now)
    - Add fallback config values
    - Add ability to set config.json path via an environment variable

--- client.go
+++ client.go
@@ -19,15 +19,17 @@ var (
 	state         = "idle"
 )
 
+// var stopPWM = make(chan bool)
+
 type Config struct {
-	Room   string
-	Server string
-	Port   string
+	Channel string
+	Server  string
+	Port    string
 }
 
 type socketClient struct {
-	ip, port, room string
-	conn           *websocket.Conn
+	ip, port, channel, status string
+	conn                      *websocket.Conn
 }
 
 type morseKey struct {
@@ -40,9 +42,103 @@ type tone struct {
 	spkrPin rpio.Pin
 }
 
+func (sc *socketClient) dial(t tone) {
+	sc.status = "dialing"
+	// start := time.Now()
+	retryEnd := time.Now().Add(time.Duration(1800) * time.Second) // try to reconnect for thirty minutes before completely giving up
+
+	for i := 1; i > 0; i++ {
+		fmt.Print("Dialing websocket (try #")
+		fmt.Print(i)
+		fmt.Print(")\n")
+		conn, err := websocket.Dial("ws://"+sc.ip+":"+sc.port+"/channel/"+sc.channel, "", "http://localhost")
+		if err == nil {
+			sc.conn = conn
+			print(sc.conn)
+			playMorse(".-. . .- -.. -.--", t)
+			sc.status = "connected"
+			break
+		}
+		if err != nil {
+			fmt.Println("Error connecting to 'ws://" + sc.ip + ":" + sc.port + "/channel/" + sc.channel + "': " + err.Error())
+			// connect to server; retry until the retry period has expired
+			if time.Now().After(retryEnd) {
+				fmt.Println("Timed out; websocket reconnection failed too many times.")
+				sc.status = "disconnected"
+				return
+			}
+			time.Sleep(time.Duration(i) * time.Second)
+			continue
+		}
+	}
+	return
+}
+
+func (t *tone) set(value int) {
+	if value == 0 {
+		t.spkrPin.Write(rpio.Low)
+		t.state = "OFF"
+
+	}
+	if value == 1 {
+		t.spkrPin.Write(rpio.High)
+		t.state = "ON"
+	} else {
+		fmt.Println("Err! Couldn’t set tone")
+	}
+}
+
+func (t *tone) start() {
+	t.spkrPin.Write(rpio.High)
+	t.state = "ON"
+
+	// This makes a very bad tone with a piezo buzzer.
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-stopPWM:
+	// 			return
+	// 		default:
+	// 			t.spkrPin.Write(rpio.High)
+	// 			time.Sleep(250 * time.Microsecond)
+	// 			t.spkrPin.Write(rpio.Low)
+	// 			time.Sleep(250 * time.Microsecond)
+	// 		}
+	// 	}
+	// }()
+}
+
+func (t *tone) stop() {
+	t.spkrPin.Write(rpio.Low)
+	// stopPWM <- true
+	t.state = "OFF"
+}
+
 func checkError(err error) {
 	if err != nil {
 		fmt.Println("Fatal error ", err.Error())
+	}
+}
+
+func playMorse(message string, t tone) {
+	speed := time.Duration(50)
+	for i := 0; i < len(message); i++ {
+		switch message[i] {
+		case 46: // == "."
+			t.start()
+			time.Sleep(speed * time.Millisecond)
+			t.stop()
+			time.Sleep(speed * time.Millisecond)
+		case 45: // == "-"
+			t.start()
+			time.Sleep(3 * speed * time.Millisecond)
+			t.stop()
+			time.Sleep(speed * time.Millisecond)
+		case 32: // == "-"
+			time.Sleep(3 * speed * time.Millisecond)
+		default:
+			// Do nothing...
+		}
 	}
 }
 
@@ -96,8 +192,16 @@ func (sc *socketClient) listen(t tone) {
 	for {
 		err := websocket.Message.Receive(sc.conn, &msg)
 		if err != nil {
+			sc.status = "disconnected"
 			fmt.Println("Couldn’t receive message: " + err.Error())
-			break
+			time.Sleep(10 * time.Second)
+			if sc.status == "dialing" {
+				fmt.Println("Currently attempting to reconnect to the websocket server.")
+			} else {
+				fmt.Println("Attempting to reconnect to websocket server in 10 seconds…")
+				time.Sleep(10 * time.Second)
+				go sc.dial(t)
+			}
 		} else {
 			state = "receiving"
 			sc.onMessage(msg, t)
@@ -105,40 +209,30 @@ func (sc *socketClient) listen(t tone) {
 	}
 }
 
-func (sc *socketClient) send(data string) {
+func (sc *socketClient) send(data string, t tone) {
+	fmt.Println("Send func")
 	err := websocket.Message.Send(sc.conn, data)
 	if err != nil {
-		fmt.Println("Sending failed:")
+		sc.status = "disconnected"
+		fmt.Println("Could not send message:")
 		fmt.Println(err.Error())
+		fmt.Println("Please double check your internet connection and telegraph configuration.")
+		if data[:1] != "1" { // Error beep on only on keyup, to prevent confusion.
+			playMorse("........", t)
+			if sc.status == "dialing" || sc.status == "connected" {
+				fmt.Println("Currently attempting to reconnect to the websocket server.")
+				fmt.Println("Current status: " + sc.status)
+			} else {
+				fmt.Println("Attempting to redial websocket server in 10 seconds")
+				fmt.Println("Current status: " + sc.status)
+				go sc.dial(t)
+			}
+		}
 	}
+	return
 }
 
-func (t *tone) start() {
-	t.spkrPin.Write(rpio.High)
-	t.state = "ON"
-}
-
-func (t *tone) stop() {
-	t.spkrPin.Write(rpio.Low)
-	t.state = "OFF"
-}
-
-func (t *tone) set(value int) {
-	if value == 0 {
-		t.spkrPin.Write(rpio.Low)
-		t.state = "OFF"
-
-	}
-	if value == 1 {
-		t.spkrPin.Write(rpio.High)
-		t.state = "ON"
-	} else {
-		fmt.Println("Err! Couldn’t set tone")
-	}
-}
-
-func (key *morseKey) listen(sc socketClient) {
-	//fmt.Println("listeningtokey")
+func (key *morseKey) listen(sc socketClient, t tone) {
 	var lastVal rpio.State = 2
 	// This is not ideal. This should be replaced with an edge detect.
 	// Waiting on https://github.com/stianeikeland/go-rpio/issues/8.
@@ -146,14 +240,14 @@ func (key *morseKey) listen(sc socketClient) {
 		val := key.keyPin.Read()
 		if val != lastVal && lastVal != 2 {
 			fmt.Println(lastVal)
-			key.keyEvent(val, sc)
+			key.keyEvent(val, sc, t)
 		}
 		lastVal = val
-		time.Sleep(200 * time.Microsecond)
+		time.Sleep(1 * time.Millisecond)
 	}
 }
 
-func (key *morseKey) keyEvent(val rpio.State, sc socketClient) {
+func (key *morseKey) keyEvent(val rpio.State, sc socketClient, t tone) {
 	fmt.Println("keyevent")
 	fmt.Println(val)
 	if state == "idle" {
@@ -166,7 +260,7 @@ func (key *morseKey) keyEvent(val rpio.State, sc socketClient) {
 		value := strconv.FormatInt(key.lastState, 10)
 		timestamp := strconv.FormatInt(key.lastDur, 10)
 		msg := value + timestamp
-		sc.send(msg)
+		go sc.send(msg, t)
 		key.lastState = 0
 		key.lastStart = microseconds()
 	}
@@ -177,52 +271,67 @@ func (key *morseKey) keyEvent(val rpio.State, sc socketClient) {
 		value := strconv.FormatInt(key.lastState, 10)
 		timestamp := strconv.FormatInt(key.lastDur, 10)
 		msg := value + timestamp
-		sc.send(msg)
+		go sc.send(msg, t)
 		key.lastState = 1
 		key.lastStart = microseconds()
 	}
+
 }
 
-func main() {
+func telegraph() {
 
-	file, _ := os.Open("config.json")
+	if len(os.Getenv("TELEGRAPH_CONFIG_PATH")) == 0 {
+		os.Setenv("TELEGRAPH_CONFIG_PATH", "config.json")
+	}
+
+	file, _ := os.Open(os.Getenv("TELEGRAPH_CONFIG_PATH"))
 	decoder := json.NewDecoder(file)
 	config := Config{}
 	err := decoder.Decode(&config)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("Error reading config.json: ", err)
+		fmt.Println("Falling back on default config…")
+		config.Channel = "lobby"
+		config.Server = "morse.autodidacts.io"
+		config.Port = "8000"
 	}
-	fmt.Println(config.Room)
+	fmt.Println(config.Channel)
 
-	sc := socketClient{ip: config.Server, port: config.Port, room: config.Room} // For internet use
-	//sc := socketClient{ip: "192.168.1.134", port: "8000"} // For intranet/LAN use
-	//sc := socketClient{ip: "127.0.0.1", port: "8000"} // For testing
-
-	conn, err := websocket.Dial("ws://"+sc.ip+":"+sc.port+"/"+sc.room, "", "http://localhost")
-	sc.conn = conn
-	checkError(err)
-
+	// Initialize morse key
 	key := morseKey{lastState: 1, lastDur: 0, lastStart: 0, lastEnd: 0}
 
+	// Init tone
 	tone := tone{state: "OFF"}
 
+	// Setup GPIO
 	openErr := rpio.Open()
-	checkError(openErr)
-
+	if openErr != nil {
+		fmt.Println("Error initializing GPIO: " + err.Error())
+	}
 	keyPn := rpio.Pin(keyPinBCM)
 	keyPn.Input()
 	spkrPn := rpio.Pin(spkrPinBCM)
 	spkrPn.Output()
 
 	tone.spkrPin = spkrPn
-
 	key.keyPin = keyPn
+
 	defer rpio.Close()
+
+	// Init socketClient & dial websocket
+	sc := socketClient{ip: config.Server, port: config.Port, channel: config.Channel}
+
+	if sc.status != "connected" && sc.status != "dialing" {
+		sc.dial(tone) // Connect!
+	}
 
 	state = "receiving"
 
-	go key.listen(sc)
+	go sc.listen(tone)
 
-	sc.listen(tone)
+	key.listen(sc, tone)
+}
 
+func main() {
+	telegraph()
 }
